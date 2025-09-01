d80f27cd4c236deb540afc1f8e04f2b6fe5ceb0f
DevFanch <devfanch44@gmail.com>
2025-09-01 16:42:52 +0200

    fix form new

--- src/Form/CourseType.php
+++ src/Form/CourseType.php
@@ -5,28 +5,52 @@ namespace App\Form;
 use App\Entity\Category;
 use App\Entity\Course;
 use App\Entity\Trainer;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use App\Repository\TrainerRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class CourseType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('content')
-            ->add('duration')
-            ->add('published')
+            ->add('name', TextType::class, [
+                'label' => 'Nom du cours',
+                'attr' => [
+                    'placeholder' => 'Entrez le nom du cours',
+                ],
+            ])
+            ->add('content', TextareaType::class, [
+                'label' => 'Contenu du cours',
+                'attr' => [
+                    'placeholder' => 'Entrez le contenu du cours',
+                ],
+            ])
             ->add('category', EntityType::class, [
                 'class' => Category::class,
-                'choice_label' => 'id',
+                'choice_label' => 'name',
+                'label' => 'Catégorie',
+                'placeholder' => '--Choisissez une catégorie--',
             ])
-            ->add('trainer', EntityType::class, [
-                'class' => Trainer::class,
-                'choice_label' => 'id',
-                'multiple' => true,
+            ->add('duration', IntegerType::class, [
+                'label' => 'Durée du cours',
+                'attr' => [
+                    'placeholder' => 'Entrez la durée du cours',
+                ],
+            ])
+            ->add('published', CheckboxType::class, [
+                'label' => 'Publié',
+                'required' => false,
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Enregistrer',
             ])
         ;
     }
