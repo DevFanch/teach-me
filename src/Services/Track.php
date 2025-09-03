53045ff0138998888ae28144f0027ab8616285b3
DevFanch <devfanch44@gmail.com>
2025-09-03 16:51:01 +0200

    Add Track Service

--- src/Services/Track.php
+++ src/Services/Track.php
@@ -6,9 +6,11 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 class Track
 {
-    
+
    public function trackUser(UserInterface $user): void {
     // Persist new email adress into file
     file_put_contents('track.txt', $user->getEmail());
-   }
+
+        $bodytag = str_ireplace("%body%", "black", "<body text=%BODY%>");
+    }
 }
